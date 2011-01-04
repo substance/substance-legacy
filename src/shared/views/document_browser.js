295f4a6586a680e981d1854b524aec838b164884
michael <ma@zive.at>
2011-01-04 16:41:11 +0100

    Show Document Status (published, draft) at the Dashboard.

--- src/shared/views/document_browser.js
+++ src/shared/views/document_browser.js
@@ -57,6 +57,7 @@ var DocumentBrowser = Backbone.View.extend({
     _.each(documents, function(doc) {
       doc.username = doc.value.data.creator.split('/')[2]
       doc.last_modified = _.prettyDate(new Date(doc.value.get('updated_at')).toJSON())
+      doc.status = doc.value.get('published_on') ? 'published' : 'draft';
     });
     
     var DESC_BY_UPDATED_AT = function(item1, item2) {
