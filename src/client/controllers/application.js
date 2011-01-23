534056d00df55368cd370a156efa07b448002675
michael <ma@zive.at>
2011-01-23 20:59:06 +0100

    Set up a #:username route. This queries for all documents within the scope of a user. Github style. ;-)

--- src/client/controllers/application.js
+++ src/client/controllers/application.js
@@ -1,11 +1,19 @@
 var ApplicationController = Backbone.Controller.extend({
   routes: {
     ':username/:docname': 'loadDocument',
-    ':username/:docname/:node': 'loadDocument'
+    ':username/:docname/:node': 'loadDocument',
+    ':username': 'userDocs'
   },
-
+  
   loadDocument: function(username, docname, node) {
+    app.browser.load({"type|=": "/type/document", "creator": "/user/"+username});
     app.document.loadDocument(username, docname, node);
+    return false;
+  },
+  
+  userDocs: function(username) {
+    username = username.length > 0 ? username : 'substance';
+    app.browser.load({"type|=": "/type/document", "creator": "/user/"+username});
     return false;
   }
 });
