ee17dbb0de4bffb2b50d3562d9a42fda3422e90f
Michael Aufreiter <ma@zive.at>
2011-03-22 20:16:33 +0100

    Clean up editors.

--- src/client/node_editors/section_editor.js
+++ src/client/node_editors/section_editor.js
@@ -7,28 +7,20 @@ var SectionEditor = Backbone.View.extend({
     var that = this;
     this.render();
     this.$node = $('#' + app.document.selectedNode.html_id + ' > .content').attr('contenteditable', true).unbind();
-
+    
     editor.activate(this.$node, {
+      placeholder: 'Enter Section Name',
       multiline: false,
       markup: false
     });
     
     editor.bind('changed', function() {
-      that.updateNode();
-    });
-  },
-  
-  updateNode: function(e) {
-    var that = this;
-    
-    setTimeout(function() {
       app.document.updateSelectedNode({
         name: editor.content()
       });
-    }, 5);
+    });
   },
   
   render: function() {
-    // $(this.el).html(Helpers.renderTemplate('edit_section', app.editor.model.selectedNode.data));
   }
 });
--- src/client/node_editors/text_editor.js
+++ src/client/node_editors/text_editor.js
@@ -12,22 +12,15 @@ var TextEditor = Backbone.View.extend({
       placeholder: 'Enter Text',
       controlsTarget: $('#document_actions')
     });
+    
     // Update node when editor commands are applied
     editor.bind('changed', function() {
-      that.updateNode();
-    });
-  },
-  
-  updateNode: function() {
-    var that = this;
-    setTimeout(function() {
       app.document.updateSelectedNode({
         content: editor.content()
       });
-    }, 5);
+    });
   },
   
   render: function() {
-    // $(this.el).html(Helpers.renderTemplate('edit_text', app.editor.model.selectedNode.data));
   }
 });
