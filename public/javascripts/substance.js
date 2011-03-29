77aabee500d6fd467bdfdca117486dedfccf52f8
Michael Aufreiter <ma@zive.at>
2011-03-29 13:46:46 +0200

    Rebuilt combined JS.

--- public/javascripts/substance.js
+++ public/javascripts/substance.js
@@ -521,7 +521,10 @@ var HTMLRenderer = function(root, parent, lvl) {
         parent: parent,
         edit: app.document.mode === 'edit',
         url: node.get('url'),
-        level: level
+        level: level,
+        empty: app.document.mode === 'edit' && (!node.get('caption') || node.get('caption') === ''),
+        caption: node.get('caption'),
+        transloadit_params: config.transloadit
       });
     },
     
@@ -848,42 +851,69 @@ var AnswerEditor = Backbone.View.extend({
 
 var ImageEditor = Backbone.View.extend({
   events: {
-    
+    'change .image-file ': 'upload'
+  },
+  
+  upload: function() {
+    console.log(this.$('.upload-image-form'));
+    this.$('.upload-image-form').submit();
   },
   
   initialize: function() {
     var that = this;
-    this.render();
     
-    $('#upload_image').transloadit({
+    this.$caption = this.$('.caption');
+    
+    editor.activate(this.$caption, {
+      placeholder: 'Enter Caption',
+      multiline: false,
+      markup: false
+    });
+    
+    editor.bind('changed', function() {
+      app.document.updateSelectedNode({
+        caption: editor.content()
+      });
+    });
+    
+    this.$('.upload-image-form').transloadit({
       modal: false,
       wait: true,
       autoSubmit: false,
       onProgress: function(bytesReceived, bytesExpected) {
-        percentage = (bytesReceived / bytesExpected * 100).toFixed(2) || 0;
-        $('#upload_progress').attr('style', 'width:' + percentage +'%');
-        $('#image_progress_legend').html('<strong>Uploading:</strong> ' + percentage + '% complete</div>');
+        var percentage = parseInt(bytesReceived / bytesExpected * 100);
+        that.$('.image-progress').show();
+        if (!(percentage >= 0)) percentage = 0;
+        
+        console.log('upload complete: '+percentage);
+        that.$('.progress-bar').attr('style', 'width:' + percentage +'%');
+        // $('#image_progress_legend').html('<strong>Uploading:</strong> ' + percentage + '% complete</div>');
       },
       onError: function(assembly) {
         console.log(assembly.error+': '+assembly.message);
-        $('#progress_container').hide();
+        alert(assembly.error+': '+assembly.message);
+        that.$('.progress-container').hide();
       },
-      onStart: function() {
-        $('#progress_container').show();
-      },
+      // onStart: function() {
+      //   that.$('.image-progress').show();
+      // },
       onSuccess: function(assembly) {
         // This triggers a node re-render
-        app.document.updateSelectedNode({
-          url: assembly.results.resize_image[0].url,
-          dirty: true
-        });
-        $('#progress_container').hide();
+        if (assembly.results.resize_image && assembly.results.resize_image[0] && assembly.results.resize_image[0].url) {
+          app.document.updateSelectedNode({
+            url: assembly.results.resize_image[0].url,
+            dirty: true
+          });
+          app.document.reset();
+          that.$('.progress-container').hide();
+        }
+        
       }
     });
   },
   
   render: function() {
-    this.$('.node-editor-placeholder').html(Helpers.renderTemplate('edit_image', app.editor.model.selectedNode.data));
+    // this.$('.node-editor-placeholder').html(Helpers.renderTemplate('edit_image', app.document.selectedNode.data));
   }
 });
 var ApplicationController = Backbone.Controller.extend({
@@ -1488,23 +1518,17 @@ var Document = Backbone.View.extend({
   
   selectNode: function(e) {
     if (this.mode === 'show') return; // Skip for show mode
-    
     var key = $(e.currentTarget).attr('name');
-    
-    this.$lead = $('#document_lead'); //.attr('contenteditable', true);
-    
-    if (!this.selectedNode || this.selectedNode.key !== key) {
+    if (!e.stopProp && (!this.selectedNode || this.selectedNode.key !== key)) {
       var node = graph.get(key);
       this.selectedNode = node;
-      // console.log(e.target);
       this.trigger('select:node', this.selectedNode);
-
+      e.stopProp = true;
       // The server will respond with a status package containing my own cursor position
       // remote.Session.selectNode(key);
     }
-    
     e.stopPropagation();
-    return false;
+    // return false;
   },
   
   publishDocument: function(e) {
@@ -2753,7 +2777,7 @@ var remote,                              // Remote handle for server-side method
     Backbone.history.start();
     
     // Reset document when window gets out of focus
-    document.body.onblur = function() {  if (app.document) app.document.reset(); }
+    // document.body.onblur = function() {  if (app.document) app.document.reset(); }
     
     // TODO: Prevent leaving page by pressing backspace
     // $('body').bind('keydown', function(e) {
