5823c9ed68ffcbfd6acc2ffdc8ed1f9cae7119d3
Michael Aufreiter <ma@zive.at>
2011-03-29 13:41:52 +0200

    The Image editor takes care of your image upload process.

--- src/client/node_editors/image_editor.js
+++ src/client/node_editors/image_editor.js
@@ -1,40 +1,67 @@
 var ImageEditor = Backbone.View.extend({
   events: {
-    
+    'change .image-file ': 'upload'
+  },
+  
+  upload: function() {
+    console.log(this.$('.upload-image-form'));
+    this.$('.upload-image-form').submit();
   },
   
   initialize: function() {
     var that = this;
-    this.render();
     
-    $('#upload_image').transloadit({
+    this.$caption = this.$('.caption');
+    
+    editor.activate(this.$caption, {
+      placeholder: 'Enter Caption',
+      multiline: false,
+      markup: false
+    });
+    
+    editor.bind('changed', function() {
+      app.document.updateSelectedNode({
+        caption: editor.content()
+      });
+    });
+    
+    this.$('.upload-image-form').transloadit({
       modal: false,
       wait: true,
       autoSubmit: false,
       onProgress: function(bytesReceived, bytesExpected) {
-        percentage = (bytesReceived / bytesExpected * 100).toFixed(2) || 0;
-        $('#upload_progress').attr('style', 'width:' + percentage +'%');
-        $('#image_progress_legend').html('<strong>Uploading:</strong> ' + percentage + '% complete</div>');
+        var percentage = parseInt(bytesReceived / bytesExpected * 100);
+        that.$('.image-progress').show();
+        if (!(percentage >= 0)) percentage = 0;
+        
+        console.log('upload complete: '+percentage);
+        that.$('.progress-bar').attr('style', 'width:' + percentage +'%');
+        // $('#image_progress_legend').html('<strong>Uploading:</strong> ' + percentage + '% complete</div>');
       },
       onError: function(assembly) {
         console.log(assembly.error+': '+assembly.message);
-        $('#progress_container').hide();
+        alert(assembly.error+': '+assembly.message);
+        that.$('.progress-container').hide();
       },
-      onStart: function() {
-        $('#progress_container').show();
-      },
+      // onStart: function() {
+      //   that.$('.image-progress').show();
+      // },
       onSuccess: function(assembly) {
         // This triggers a node re-render
-        app.document.updateSelectedNode({
-          url: assembly.results.resize_image[0].url,
-          dirty: true
-        });
-        $('#progress_container').hide();
+        if (assembly.results.resize_image && assembly.results.resize_image[0] && assembly.results.resize_image[0].url) {
+          app.document.updateSelectedNode({
+            url: assembly.results.resize_image[0].url,
+            dirty: true
+          });
+          app.document.reset();
+          that.$('.progress-container').hide();
+        }
+        
       }
     });
   },
   
   render: function() {
-    this.$('.node-editor-placeholder').html(Helpers.renderTemplate('edit_image', app.editor.model.selectedNode.data));
+    // this.$('.node-editor-placeholder').html(Helpers.renderTemplate('edit_image', app.document.selectedNode.data));
   }
 });
