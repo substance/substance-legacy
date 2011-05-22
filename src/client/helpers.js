dfa555a57240532a118d26cf701cab911652a81b
Michael Aufreiter <ma@zive.at>
2011-05-22 23:32:42 +0200

    Create slug from document title.

--- src/client/helpers.js
+++ src/client/helpers.js
@@ -44,6 +44,25 @@ Helpers.renderTemplate = _.renderTemplate = function(tpl, view, helpers) {
   return template(view, helpers || {});
 };
 
+
+_.slug = function(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+  
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to   = "aaaaeeeeiiiioooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+}
+
 // Render Underscore templates
 _.tpl = function(tpl, ctx) {
   source = $("script[name="+tpl+"]").html();
