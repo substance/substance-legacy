435b4ca96e1b3aa0e413174c3f171d49193b91d3
Michael Aufreiter <ma@zive.at>
2011-07-28 18:37:48 +0200

    Updated Data.js to 0.4.1

--- db/seed.js
+++ db/seed.js
@@ -6,7 +6,7 @@ var _ = require('underscore');
 var config = JSON.parse(fs.readFileSync(__dirname+ '/../config.json', 'utf-8'));
 var seed = JSON.parse(fs.readFileSync(__dirname+ '/schema.json', 'utf-8'));
 
-var graph = new Data.Graph(seed, true);
+var graph = new Data.Graph(seed, {dirty: true});
 
 // Setup Data.Adapter
 graph.connect('couch', { url: config.couchdb_url, force_updates: true });
