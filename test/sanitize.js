40360934890011a831a184cdca530051fdea3ca6
Tim Baumann <tim@timbaumann.info>
2011-09-24 23:01:42 +0200

    Escape quotes in sanitize

--- test/sanitize.js
+++ test/sanitize.js
@@ -8,5 +8,7 @@ assert.equal(sanitize('<a b="c"><d>lorem</d> ipsum </a>dolor', {a:{b:true}}), '<
 assert.equal(sanitize('<a b="c"><d>lorem</d> ipsum </a>dolor', {a:{b:true}}), '<a b="c">lorem ipsum </a>dolor');
 assert.equal(sanitize('<a b="c"><d>lorem</d> ipsum </a>dolor', {a:{b:function (v){return v==='c'}}}), '<a b="c">lorem ipsum </a>dolor');
 assert.equal(sanitize('<a b="x"><d>lorem</d> ipsum </a>dolor', {a:{b:function (v){return v==='c'}}}), '<a>lorem ipsum </a>dolor');
+assert.equal(sanitize('" onclick="alert(true);'), '&quot; onclick=&quot;alert(true);');
+assert.equal(sanitize("<a href='\" onclick=\"alert(true);'>", {a:{href:true}}), '<a href="&quot; onclick=&quot;alert(true);">');
 
 console.log("All tests have completed successfully!");
