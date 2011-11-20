64e1b281a15c29cb151a5ca409d7b036747f8ee7
Tim Baumann <tim@timbaumann.info>
2011-11-20 21:58:31 +0100

    Fixing quote styles. Closes #143

--- src/client/views/node/quote.js
+++ src/client/views/node/quote.js
@@ -8,8 +8,10 @@ Node.define('/type/quote', {
     Node.prototype.render.apply(this);
     
     var blockquote = $('<blockquote />').appendTo(this.contentEl);
-    this.quoteContentEl = this.makeEditable($('<p class="quote-content" />'), 'content', "Enter Quote").appendTo(blockquote);
-    this.quoteAuthorEl  = this.makeEditable($('<cite class="quote-author" />'), 'author', "Enter Author").appendTo(blockquote);
+    this.quoteContentEl = this.makeEditable($('<p class="quote-content" />'), 'content', "Enter Quote")
+      .appendTo($('<div />').appendTo(blockquote));
+    this.quoteAuthorEl  = this.makeEditable($('<cite class="quote-author" />'), 'author', "Enter Author")
+      .appendTo($('<div />').appendTo(blockquote));
     $('<br clear="both" />').appendTo(this.contentEl);
     
     return this;
