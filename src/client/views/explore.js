3c627777023c070ea95cbd68675fbe330be26ca7
Michael Aufreiter <ma@zive.at>
2011-12-19 17:06:35 +0100

    Load some documents when a network is selected.

--- src/client/views/explore.js
+++ src/client/views/explore.js
@@ -1,17 +1,26 @@
 s.views.Explore = Backbone.View.extend({
+  events: {
+  	'click .network': '__toggleNetwork'
+  },
 
+  __toggleNetwork: function(e) {
+  	this.updateResults($(e.currentTarget).attr('data-network'));
+  	return false;
+  },
 
   // For a particular network selection load results
-  loadResults: function(network) {
-  	this.results = new s.views.Results({ model: this.model, id: "results" });
+  updateResults: function(network) {
+  	var that = this;
+
+    loadDocuments({"type": "user", "value": "michael"}, function (err, data) {
+      that.results = new s.views.UserBrowser({ model: data, el: "#results" }).render();
+    });
   },
 
   render: function() {
     $(this.el).html(s.util.tpl('explore', {
       networks: this.model.networks
     }));
-    
-    if (this.results) this.$(this.results.render().el).appendTo(this.el);
     return this;
   }
 });
