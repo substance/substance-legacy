8c0939131482973632941aec526689452cba8632
Oliver Buchtala <oliver@Oliver-Buchtalas-Mac-Pro.local>
2012-09-21 00:40:53 +0200

    Add a WebView to the osx app.

--- app/osx/appDelegate.h
+++ app/osx/appDelegate.h
@@ -1,9 +1,12 @@
 #import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
 
 @interface SubstanceAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
+	WebView *webView;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+@property (nonatomic, retain) IBOutlet WebView *webView;
 
 @end
--- app/osx/appDelegate.m
+++ app/osx/appDelegate.m
@@ -3,9 +3,13 @@
 @implementation SubstanceAppDelegate
 
 @synthesize window;
+@synthesize webView;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application 
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {	
+	NSString *url = @"http://www.google.at";
+	[ [webView mainFrame] loadRequest: 
+		[NSURLRequest requestWithURL: [NSURL URLWithString:url] ]
+	];
 }
 
 @end
