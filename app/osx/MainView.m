6c1db287b3491e2afa081118883540eb39e13866
Oliver Buchtala <oliver@Oliver-Buchtalas-Mac-Pro.local>
2012-09-27 22:42:44 +0200

    Disable noisy keys.

---
+++ app/osx/MainView.m
@@ -0,0 +1,43 @@
+//
+//  MainView.m
+//  substance
+//
+//  Created by Oliver Buchtala on 9/28/12.
+//
+//
+
+#import "MainView.h"
+
+@implementation MainView
+
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+    [super drawRect:dirtyRect];
+}
+
+- (BOOL)acceptsFirstResponder {
+    return YES;
+}
+
+- (void) keyDown:(NSEvent *)theEvent {
+    // Note: this consumes all key events that have not been handled
+    //       by within the child views (e.g. WebView)
+    //       if some application wide key handling is necessary, handle the keyEvent
+    //[self interpretKeyEvents: [NSArray arrayWithObject:theEvent]];
+}
+
+@end
