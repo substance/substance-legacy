7408be92e11dc56cdfab1f618db318c764bed946
Michael Aufreiter <michael@substance.io>
2012-11-02 23:29:58 +0100

    Calculate proper comment count.

--- src/nodes/node.js
+++ src/nodes/node.js
@@ -36,7 +36,7 @@ sc.views.Node = Dance.Performer.extend(_.extend({}, s.StateMachine, {
   focus: function () {},
 
   render: function () {
-    var cnt = this.document.comments(this.model.id).length;
+    var cnt = this.document.commentCount(this.model.id);
     $(this.el).html('<div class="content" contenteditable="true"></div><div class="handle"></div><div class="handle-2"></div><a href="#" class="comments-toggle'+(cnt > 0 ? ' active' : '')+'">'+cnt+'</a>');
     return this;
   }
