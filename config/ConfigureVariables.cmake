4f17999401fa28db0bddbf69edec59ec18685e67
Oliver Buchtala <oliver.buchtala@googlemail.com>
2012-11-03 02:18:47 +0100

    Use PkgConfig and FindexWidgets.
    Aslo set global configuration variables in extra cmake module.

---
+++ config/ConfigureVariables.cmake
@@ -0,0 +1,22 @@
+set(BOOST_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/ext/boost)
+
+set(GTEST_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/ext/gtest/gtest/include)
+set(GTEST_LIB_DIRS ${PROJECT_SOURCE_DIR}/ext/gtest/bin)
+set(GTEST_LIBS gtest gtest_main)
+
+set(HIREDIS_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/ext/hiredis/hiredis)
+set(HIREDIS_LIB_DIRS "${PROJECT_SOURCE_DIR}/ext/hiredis/hiredis")
+set(HIREDIS_LIBS -lhiredis)
+
+IF (NOT APPLE AND UNIX)
+
+  # use pkg-config module to configure webkit-1.0
+  include(FindPkgConfig)
+  pkg_check_modules (webkit webkit-1.0 REQUIRED)
+
+  # use wxWidgets configuration as described here: http://wiki.wxwidgets.org/CMake
+  include(FindwxWidgets)
+  FIND_PACKAGE(wxWidgets COMPONENTS core base webview REQUIRED)
+  include("${wxWidgets_USE_FILE}")
+
+ENDIF () # NOT APPLE AND UNIX
