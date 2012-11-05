52f254b4f9e58731146e4db183dd6e23cc259553
Michael Aufreiter <michael@substance.io>
2012-11-05 19:22:33 +0100

    Allow annotations/comments sticking on heading nodes. Closes #232

--- src/nodes/heading/heading.js
+++ src/nodes/heading/heading.js
@@ -16,6 +16,12 @@ sc.views.Node.define('heading', {
     $(this.el).remove();
   },
 
+  annotate: function(type) {
+    var id = "annotation:"+Math.uuid();
+    this.surface.insertAnnotation({ id: id, type: type, pos: this.surface.selection() });
+    choreographer.trigger('comment-scope:selected', id, this.model.id, id);
+  },
+
   transitionTo: function (state) {
     sc.views.Node.prototype.transitionTo.call(this, state);
     if (this.state === state) {
@@ -26,9 +32,26 @@ sc.views.Node.define('heading', {
   initSurface: function() {
     var that = this;
 
+    var annotations = app.view.model.document.annotations(this.model.id);
+
     this.surface = new Substance.Surface({
       el: this.$('.content')[0],
-      content: this.model.content
+      content: this.model.content,
+      annotations: annotations,
+      types: {
+        "idea": {
+          "inclusive": false,
+          "visibility" : 'both'
+        },
+        "blur": {
+          "inclusive": false,
+          "visibility" : 'both'
+        },
+        "doubt": {
+          "inclusive": false,
+          "visibility" : 'both'
+        }
+      }
     });
 
     // Events
@@ -43,13 +66,39 @@ sc.views.Node.define('heading', {
       app.view.model.select([that.model.id], {edit: true});
     });
 
-    this.surface.on('content:changed', function(content, prevContent) {
+    function selectionChanged(sel) {
+      var marker = that.surface.getAnnotations(sel, ["idea", "blur", "doubt"])[0];
+      if (marker) {
+        choreographer.trigger('comment-scope:selected', marker.id, that.model.id, marker.id);
+        that.surface.highlight(marker.id);
+      } else {
+        choreographer.trigger('comment-scope:selected', 'node_comments', that.model.id, null);
+        that.surface.highlight(null);
+      }
+    }
+
+    // Update comments panel according to marker context
+    this.surface.off('selection:changed', selectionChanged);
+    this.surface.on('selection:changed', selectionChanged);
+
+    this.surface.on('annotations:changed', function() {
+      that.session.comments.updateAnnotations(that.surface.getContent(), that.surface.annotations);
+    });
+
+
+    this.surface.on('content:changed', function(content, prevContent, ops) {
       var delta = _.extractOperation(prevContent, content);
 
-      console.log("Partial Text Update", delta);
-
-      var op = ["update", {id: that.model.id, "data": delta}];
-      that.document.apply(op, {user: "michael"});
+      // Update content incrementally
+      if (content !== prevContent) {
+        that.document.apply(["update", {id: that.model.id, "data": delta}]);
+      }
+      // Applying annotation ops...
+      _.each(ops, function(op) {
+        op[0] += "_annotation"; // should be done on the surface level?
+        op[1].node = that.model.id;
+        that.document.apply(op, {user: "michael"});
+      });
     });
   },
 
