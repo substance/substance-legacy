ec9eb94fe21102bbd5a8f4ba74071cf4781d1a9e
Oliver Buchtala <oliver.buchtala@googlemail.com>
2012-12-11 22:46:11 +0100

    Allow back'n'forward navigation using ctrl+alt+cmd + left/right.

--- app/osx/MainView.m
+++ app/osx/MainView.m
@@ -21,14 +21,36 @@
 }
 
 - (void) keyDown:(NSEvent *)theEvent {
+
+  unsigned int flags = [theEvent modifierFlags];
   // Note: this consumes all key events that have not been handled
   //       by within the child views (e.g. WebView)
   //       if some application wide key handling is necessary, handle the keyEvent
-  if([theEvent modifierFlags] & NSCommandKeyMask) {
+  if( flags & NSCommandKeyMask) {
 
     // Reload on Command + r
     if([[theEvent characters] characterAtIndex:0] == 'r') {
       [m_webView reload: self];
+    }
+
+    // Command+Ctrl+Alt
+    if(flags & (NSControlKeyMask | NSAlternateKeyMask)) {
+
+      if(flags & NSNumericPadKeyMask) {
+        NSString *theArrow = [theEvent charactersIgnoringModifiers];
+        if ([theArrow length] == 1) {
+          unichar keyChar = [theArrow characterAtIndex:0];
+
+          // Command+Ctrl+Alt + left = Back
+          if (keyChar == NSLeftArrowFunctionKey) {
+            [m_webView goBack];
+          }
+          // Command+Ctrl+Alt + right = Forward
+          else if (keyChar == NSRightArrowFunctionKey) {
+            [m_webView goForward];
+          }
+        }
+      }
     }
   }
 
