611b955791f7715ad6b7303bc9937e3771029c82
Michael Aufreiter <michael@substance.io>
2013-01-25 18:46:10 +0100

    Much improved, more tested Replicator.

--- src/replicator.js
+++ src/replicator.js
@@ -7,7 +7,6 @@ Substance.Replicator = function(params) {
   var that = this;
 
   // Util
-
   function extractCommits(doc, start, end) {
     var skip = false;
     
@@ -29,21 +28,22 @@ Substance.Replicator = function(params) {
         prev = commit;
       }
     }
-
     return commits.reverse();
   }
+
 
   // Start sync
   // -----------------
 
   this.sync = function(cb) {
-  	this.computeDiff(function(err, jobs) {
-      console.log('JOBS', jobs);      
+  	this.computeJobs(function(err, jobs) {
+      console.log('JOBS', jobs);
   		var index = 0;
 
   		function next() {
   			if (index === jobs.length) return cb(null);
   			that.processDocument(jobs[index], function(err) {
+          if (err) console.log('ERROR WHILE PROCSSING JOB', jobs[index], "ERROR", err);
   				index += 1;
   				next();
   			});
@@ -58,17 +58,23 @@ Substance.Replicator = function(params) {
 
   this.processDocument = function(doc, cb) {
     if (doc.action === "create") return that.create(doc, cb);
-    if (doc.action === "update") return that.update(doc, cb);
+    if (doc.action === "push") return that.push(doc, cb);
+    if (doc.action === "pull") return that.pull(doc, cb);
     if (doc.action === "fetch") return that.fetch(doc, cb);
   };
 
   // Fetch doc from remote store
   // -----------------
+  // 
+  // TODO: create dedicated fetch method on server, returning commits in a sequence
+  // so we don't have to use extractCommits
 
   this.fetch = function(doc, cb) {
     console.log('fetching ', doc.id, ' from server');
     _.request("GET", Substance.settings.hub + '/documents/get/'+that.user+'/'+doc.id, {}, function(err, doc) {
 
+      console.log('THE DOC, just fetched', doc);
+      // var commits = store.commits(doc.id, doc.refs.tail);
       var commits = extractCommits(doc, doc.refs.tail);
       console.log('commits to be applied locally', commits);
 
@@ -85,7 +91,11 @@ Substance.Replicator = function(params) {
           console.log('updating local metadata from server', doc.meta);
 
           store.updateMeta(doc.id, doc.meta, function(err) {
-            store.setRef(doc.id, 'synced', doc.refs.tail);
+            store.setRef(doc.id, 'tail-remote', doc.refs.tail);
+            store.setRef(doc.id, 'tail-master', doc.refs.master);
+            // In case there are some undone commits
+            store.setRef(doc.id, 'master', doc.refs.master);
+
             cb(err);
           });
         });
@@ -97,25 +107,38 @@ Substance.Replicator = function(params) {
   // Pull
   // -----------------
   // 
-  // TODO: Fetch metadata on pull and update locally
+  // TODO: Fetch metadata on pull and update locally / this overwrites pending local commits
 
   this.pull = function(doc, cb) {
     // Get latest synced commit
-    var synced_commit = store.getRef(doc.id, 'synced');
-    console.log('pulling in changes...');
+    var tailRemote = store.getRef(doc.id, 'tail-remote');
+    console.log('pulling in changes... for', tailRemote);
 
-    _.request("GET", Substance.settings.hub + '/documents/commits/'+that.user+'/'+doc.id+'/'+synced_commit, {}, function(err, commits) {
-      console.log('remote commits', commits);
-      if (commits.length > 0) {
-        store.update(doc.id, commits, function(err) {
+    _.request("GET", Substance.settings.hub + '/documents/commits/'+that.user+'/'+doc.id+'/'+tailRemote, {}, function(err, data) {
+      console.log('what does remote say?', data);
+      console.log('remote commits', data.commits);
+      // Should also give me remote refs
+      // data.commits |data.refs
+      if (data.commits.length > 0) {
+        store.update(doc.id, data.commits, function(err) {
           console.log('applied remote commits.. Any ERRORS?', err);
 
-          // Tail and master are now up to date. Now set 'synced' to new tail
-          store.setRef(doc.id, 'synced', store.getRef(doc.id, 'tail'));
-          cb(null, false);
+          // Update references
+          store.setRef(doc.id, 'master', data.refs.master);
+          store.setRef(doc.id, 'tail', data.refs.tail); // now done implicitly by update
+
+          // Tail and master are now up to date. Now set 'tail-remote' to new tail
+          store.setRef(doc.id, 'tail-remote', data.refs.tail);
+          store.setRef(doc.id, 'master-remote', data.refs.master);
+
+          // Update metadata (remote version)
+          store.updateMeta(doc.id, data.meta);
+          cb(null);
         });
       } else {
-        cb(null, true);
+        // Update metadata (remote version)
+        store.updateMeta(doc.id, data.meta);
+        cb(null);
       }
     });
   };
@@ -126,17 +149,28 @@ Substance.Replicator = function(params) {
 
   this.push = function(doc, cb) {
     store.get(doc.id, function(err, doc) {
-      var syncedCommit = store.getRef(doc.id, 'synced');  
+      var remoteTail = store.getRef(doc.id, 'tail-remote');  
       var localTail = store.getRef(doc.id, 'tail');
 
-      var commits = extractCommits(doc, localTail, syncedCommit);
-      if (commits.length === 0) return cb(null); // do nothing
+      // Find all commits after synced (remote) commit
+      var commits = store.commits(doc.id, localTail, remoteTail);
 
-      // Send new local commits to server
-      _.request("POST", Substance.settings.hub + '/documents/update', {"username": that.user, id: doc.id, commits: commits, meta: doc.meta}, function (err) {
+      console.log('LE LOCAL COMMITTES to be pushed: ', commits);
+
+      var data = {
+        username: that.user, 
+        id: doc.id,
+        commits: commits, // may be empty
+        meta: doc.meta,
+        refs: doc.refs // make sure refs are updated on the server (for now master, tail is updated implicitly)
+      };
+
+      _.request("POST", Substance.settings.hub + '/documents/update', data, function (err) {
         // Set synced reference accordingly
         if (err) return cb(err);
-        store.setRef(doc.id, 'synced', doc.refs.tail);
+        store.setRef(doc.id, 'tail-remote', doc.refs.tail);
+        store.setRef(doc.id, 'master-remote', doc.refs.master);
+
         cb(null);
       });
     });
@@ -145,19 +179,19 @@ Substance.Replicator = function(params) {
   // Sync doc with remote store (bi-directional)
   // -----------------
 
-  this.update = function(doc, cb) {
-    that.pull(doc, function(err, fastForward) {
-      // For now only fast forward sync works smoothly
-      // In case ther are local changes, they are lost (not pushed! what ever is on the server wins)
-      if (fastForward) {
-        that.push(doc, cb);
-        console.log('pull successfull. no changes.. fast forwarding..');  
-      } else {
-        console.log('pull with changes ignoring local changes');
-        cb(null); // done
-      }
-    });
-  };
+  // this.update = function(doc, cb) {
+  //   that.pull(doc, function(err, fastForward) {
+  //     // For now only fast forward sync works smoothly
+  //     // In case ther are local changes, they are lost (not pushed! what ever is on the server wins)
+  //     if (fastForward) {
+  //       that.push(doc, cb);
+  //       console.log('pull successfull. no changes.. fast forwarding..');  
+  //     } else {
+  //       console.log('pull with changes ignoring local changes');
+  //       cb(null); // done
+  //     }
+  //   });
+  // };
 
   // Create doc on the server
   // -----------------
@@ -168,16 +202,16 @@ Substance.Replicator = function(params) {
     // 1. Get doc from local docstore
     store.get(doc.id, function(err, doc) {
       // extract commits
-      var commits = extractCommits(doc, doc.refs.tail);
-
-      // console.log("COMMITS", commits);
+      var commits = store.commits(doc.id, doc.refs.tail);
+      console.log("COMMITS OF NEW DOC", commits);
       // 2. Create empty doc on the server
-      _.request("POST", Substance.settings.hub + '/documents/create', {"username": that.user, id: doc.id, meta: doc.meta}, function (err) {
+      _.request("POST", Substance.settings.hub + '/documents/create', {"username": that.user, id: doc.id}, function (err) {
         console.log('created on the server..');
         if (err) return cb(err);
         // 3. Send updates to server
-        _.request("POST", Substance.settings.hub + '/documents/update', {"username": that.user, id: doc.id, commits: commits}, function (err) {
-          store.setRef(doc.id, 'synced', doc.refs.tail);
+        _.request("POST", Substance.settings.hub + '/documents/update', {"username": that.user, id: doc.id, commits: commits, meta: doc.meta}, function (err) {
+          store.setRef(doc.id, 'tail-remote', doc.refs.tail);
+          store.setRef(doc.id, 'master-remote', doc.refs.master);
           cb(err);
         });
       });
@@ -185,10 +219,10 @@ Substance.Replicator = function(params) {
   };
 
 
-  // Compute dirty documents
+  // Compute jobs for dirty documents
   // -----------------
 
-  this.computeDiff = function(cb) {
+  this.computeJobs = function(cb) {
   	// Status object looks like this
     var jobs = [];
     that.localDocStates(function(err, localDocs) {
@@ -203,9 +237,16 @@ Substance.Replicator = function(params) {
             return;
           }
 
-          // Remote changes || local changes
-          if (doc.refs['synced'] !== remoteDoc.refs['tail'] || doc.refs['synced'] !== doc.refs['tail']) {
-            jobs.push({id: id, action: "update"});
+          // Local refs
+          var masterRemote = store.getRef(id, 'master-remote');
+          var tailRemote = store.getRef(id, 'tail-remote');
+
+          if (tailRemote !== remoteDoc.refs['tail'] || masterRemote !== remoteDoc.refs['master']) {
+            jobs.push({id: id, action: "pull"});
+            return;
+          } else if (tailRemote !== doc.refs['tail'] || doc.refs['master'] !== masterRemote) {
+            // Local changes only -> Push (fast-forward)
+            jobs.push({id: id, action: "push"});
             return;
           }
         });
@@ -225,20 +266,11 @@ Substance.Replicator = function(params) {
     });
   };
 
+
   // Ask for status of all local documents
   // -----------------
   // 
   // Returns: A hash of document status objects
-  // 
-	// {
-	// 	"doc-1": {
-  // 		"refs": {
-  // 			"master": "commit-25",
-  // 			"tail": "commit-28"
-  // 		}
-	// 	},
-	// 	"doc-": ...
-	// }
 
   this.localDocStates = function(cb) {
     store.list(function(err, localDocs) {
