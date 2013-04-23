0536a49fc07131394bcfac4b69efdd33fbad00f6
Michael Aufreiter <michael@substance.io>
2013-04-23 19:36:34 +0200

    Improved testsuite view.

--- src/views/testsuite.js
+++ src/views/testsuite.js
@@ -11,14 +11,19 @@ sc.views.Testsuite = Backbone.View.extend({
     var that = this;
     var test = Substance.tests[testName];
 
+    that.$('.test-results').empty();
     that.$('.test-results').removeClass('error success');
-    test.run(function(err) {
-      if(err) {
 
-        that.$('.test-results').html(err.toString()+'<br/><pre>'+JSON.stringify(err, null, '  ')+'</pre>').addClass('error');
-      } else {
-        that.$('.test-results').html('Success').addClass('success');
-      }
+    test.on('action:success', function(err, action) {
+      that.$('.test-results').append('<div class="action-successful">'+action.label.join(', ')+'. Success</div>');
+    });
+
+    test.on('action:error', function(err, action) {
+      that.$('.test-results').append('<div class="action-error">'+err.toString()+'<br/><pre>'+JSON.stringify(err, null, '  ')+'</pre></div>');
+    });
+
+    test.run(function(err) {
+
     });
 
   },
