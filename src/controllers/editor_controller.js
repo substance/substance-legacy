d9837ed699b76d7755907eb740c4b354d3d5aaf7
vectorsize <vectorsize@gmail.com>
2013-07-27 15:17:50 +0200

    vertical navigation in place

--- src/controllers/editor_controller.js
+++ src/controllers/editor_controller.js
@@ -11,24 +11,99 @@ var util = require('substance-util');
 // -----------------
 //
 
-var LineBehavior = function(selection, editorView) {
+var LineBehavior = function(selection, surface) {
 
   var __find__ = selection.find;
+  var __set__ = selection.set;
+
+  var verticalNav = false;
+  var iniX;
+
+  var getOL = function (el) {
+    var rect = el.getClientRects();
+    return rect[0].left;
+  };
+
+  var resetCursor = function() {
+    var span = getSpan();
+    if(!span) return;
+    iniX = getOL(span);
+    console.log('span', span);
+    console.log('iniX', iniX);
+  };
+
+  var getSpan = function() {
+    var sel = selection.getCursor();
+    if(!sel) return;
+    var nodeEl = surface.el.children[sel[0]];
+    var content = nodeEl.children[0]; // [1] is the cursor div
+    var span = content.children[sel[1]];
+    return span;
+  };
+
+  var upDown = function(direction) {
+
+    var sel = selection.getCursor();
+    var node = sel[0];
+    var charOffset = sel[1];
+    var foundline = false;
+    var span = getSpan();
+    var iniY = span.offsetTop;
+
+    if (iniX === undefined) {
+      iniX = getOL(span);
+    }
+
+    var go = (direction === "down") ? span.nextSibling: span.previousSibling;
+    
+    while (go) {
+
+      if (direction === "down") {
+        span = span.nextSibling;
+        charOffset ++;
+      } else {
+        span = span.previousSibling;
+        charOffset --;
+      }
+
+      if (span.offsetTop !== iniY) {
+        foundline = true;
+      }
+
+      if (direction === "down") {
+        if (foundline && getOL(span) > iniX) {
+          return [node, charOffset];
+        }
+      } else {
+        if (foundline && getOL(span) < iniX) {
+          return [node, charOffset+1];
+        }
+      }
+    }
+  };
 
   selection.prevLine = function(pos) {
-    console.log('finding prev line');
-    // use editorView
-    return pos;
+    return upDown('up');
   };
 
   selection.nextLine = function(pos) {
-    console.log('do stuff next-line');
-    // use editorView
-    return pos;
+    return upDown('down');
+  };
+
+  selection.set = function(pos, direction, granularity) {
+
+    __set__.call(this, pos, direction, granularity);
+    
+    if(!verticalNav) {
+      resetCursor();
+    }
+    verticalNav = false;
+    
   };
 
   selection.find = function(pos, direction, granularity) {
     if (granularity === "line") {
+      verticalNav = true;
       if (direction === "left") {
         return this.prevLine(pos);
       } else {
@@ -61,9 +136,10 @@ EditorController.Prototype = function() {
   this.createView = function() {
     this.writer = new Document.Writer(this.__document);
     var view = new EditorView(this);
+    var surface = view.surface;
 
     // Mixin Line behavior into selection object
-    LineBehavior(this.writer.selection, view);
+    LineBehavior(this.writer.selection, surface);
     return view;
   };
 
