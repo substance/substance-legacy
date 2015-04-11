5a03f1d97a128e657e603aad7724cd55bcb4fc60
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-04-11 03:18:44 +0200

    Cleanup.

--- src/writer/components/annotation_handle.js
+++ src/writer/components/annotation_handle.js
@@ -4,9 +4,6 @@ var Substance = require('substance');
 var View = require('./view');
 var DomSelection = Substance.Surface.DomSelection;
 
-// TODO: the old implementation is here:
-// https://github.com/michael/substance-composer/blob/09d9048c9bfaad2cbc0df72a3e292eb189cffc51/src/workflows/edit_annotation_range.js
-
 var AnnotationHandle = View.extend({
   displayName: "AnnotationHandle",
 
@@ -15,14 +12,8 @@ var AnnotationHandle = View.extend({
       .addClass(this.props.node.getClassNames().replace(/_/g, '-'))
       .addClass(this.props.classNames.join(' '))
       .attr('data-id', this.props.node.id);
-    // var $caret = $('<span>').addClass('anchor-caret');
-    // $element.append($caret);
-
     this.$element = $element;
     this.element = $element[0];
-    // this.$caret = $caret;
-
-    // NOTE: we deactivated the draggable handle implementation.
     return this.element;
   },
 
