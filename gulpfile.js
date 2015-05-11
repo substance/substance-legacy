d823a7e9faeebffea393508366e2c7ff2d535e4d
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-05-11 20:03:00 +0200

    Gulp script.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var argv = require('yargs').argv;
+var gulpif = require('gulp-if');
+var rename = require('gulp-rename');
+var jshint = require('gulp-jshint');
+var yuidoc =  require('gulp-yuidoc');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('doc', function() {
+  return gulp.src("./src/**/*.js")
+    .pipe(yuidoc.parser())
+    .pipe(yuidoc.reporter())
+    .pipe(yuidoc.generator())
+    .pipe(gulp.dest('./doc/api'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('build', ['lint'], function() {
+  return browserify({
+      entries: './browser.js',
+      debug: true
+    }).bundle()
+    .pipe(source('substance.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(gulpif(argv.production, uglify()))
+    .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['build']);
