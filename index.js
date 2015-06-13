3cc1a7a5bd292710ba9ce96a4619013799126603
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-06-13 16:21:59 +0200

    Expose helpers.

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ Substance.Document = require('./src/document');
 Substance.Operator = require('./src/operator');
 Substance.Surface = require('./src/surface');
 
+Substance._ = require('./src/basics/helpers');
+
 module.exports = Substance;
