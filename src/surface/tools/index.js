27ea40286043b73f752cc81c843dda58d73b2186
Michael Aufreiter <michael@substance.io>
2015-07-17 20:58:00 +0200

    Include undo/redo tool.

--- src/surface/tools/index.js
+++ src/surface/tools/index.js
@@ -1,4 +1,6 @@
 module.exports = {
+  Undo: require('./undo_tool'),
+  Redo: require('./redo_tool'),
   Emphasis: require('./emphasis_tool'),
   Strong: require('./strong_tool'),
   Link: require('./link_tool'),
