f4bbd4eed7b77399ce50fa52e78e4c02629515d3
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-07-31 16:18:51 +0200

    Preservative rerendering.

--- ui/component.js
+++ ui/component.js
@@ -25,26 +25,24 @@ function Component(parent, props) {
   if (props) {
     if (props.key) {
       this.keyId = props.key;
-      delete props.key;
     }
     if (props.ref) {
       this.refId = props.ref;
-      delete props.ref;
     }
   }
 
   this.parent = parent;
-  this.children = [];
-  this._childrenById = {};
+  this.content = [];
+  this.children = {};
   this.refs = {};
 
   this._setProps(props);
   this._setState(this.getInitialState());
   this.$el = this.createElement();
+  this.setAttributes();
+
   // gather context from parent components (dependency injection)
   this.context = this._collectContext();
-
-  this._virgin = true;
 }
 
 Component.Prototype = function ComponentPrototype() {
@@ -64,17 +62,28 @@ Component.Prototype = function ComponentPrototype() {
   };
 
   this.createElement = function() {
-    var $el = $('<' + this.tagName + '>');
-    $el.addClass(this.classNames);
+    this.$el = $('<' + this.tagName + '>');
+    this.$el.addClass(this.classNames);
     if (this.props.classNames) {
-      $el.addClass(this.props.classNames);
+      this.$el.addClass(this.props.classNames);
     }
+    return this.$el;
+  };
+
+  this.setAttributes = function() {
     _.each(this.props, function(val, key) {
       if (key.slice(0, 5) === "data-") {
-        $el.attr(key, val);
+        this.$el.attr(key, val);
       }
-    });
-    return $el;
+    }, this);
+  };
+
+  this.removeAttributes = function() {
+    _.each(this.props, function(val, key) {
+      if (key.slice(0, 5) === "data-") {
+        this.$el.removeAttr(key, val);
+      }
+    }, this);
   };
 
   /**
@@ -91,17 +100,7 @@ Component.Prototype = function ComponentPrototype() {
   };
 
   this.rerender = function() {
-    var oldChildren = this._childrenById;
-    var content = this._compileContent(this.render());
-    this._renderContent(content);
-    // automatical unmounting unused children
-    _.each(oldChildren, function(child, key) {
-      if (!this._childrenById[key]) {
-        console.log('Automatically unmounting', child);
-        child.unmount();
-      }
-    }, this);
-    return this;
+    this._renderContent(this.render());
   };
 
   /**
@@ -111,8 +110,7 @@ Component.Prototype = function ComponentPrototype() {
    * on this component and all of its children.
    */
   this.mount = function($el) {
-    var content = this._compileContent(this.render());
-    this._renderContent(content);
+    this._renderContent(this.render());
     $el.append(this.$el);
     // trigger didMount automatically if the given element is already in the DOM
     if (_isInDocument($el[0])) {
@@ -242,89 +240,229 @@ Component.Prototype = function ComponentPrototype() {
     return false;
   };
 
-  this._compileContent = function(contentData) {
-    if (!_.isArray(contentData)) {
-      contentData = [contentData];
+  function _indexByKey(content, old) {
+    var index = {};
+    for (var i = 0; i < content.length; i++) {
+      var key;
+      // yuck
+      if (old) {
+        key = content[i].component.keyId;
+      } else {
+        key = content[i].props.key;
+      }
+      if (key) {
+        index[key] = content[i];
+      }
     }
-    var content = [];
-    _.each(contentData, function(data) {
-      if (_.isString(data)) {
-        content.push(data);
-        return;
+    return index;
+  }
+
+  function _prepareContent(content) {
+    if (!_.isArray(content)) {
+      content = [ content ];
+    }
+    // preparations for text nodes
+    for (var i = 0; i < content.length; i++) {
+      if (_.isString(content[i])) {
+        content[i] = {
+          type: 'text',
+          props: {},
+          text: content[i]
+        };
       }
-      var child = null;
-      var childContent = null;
-      if (data.props && data.props.key && this._childrenById[data.props.key]) {
-        child = this._childrenById[data.props.key];
-      }
-      if (data.type === 'element') {
-        // FIXME: this temporarily removes DOM elements of preserved components
-        // which would have the effect of loosing the selection in an editor
-        // for instance. We need a minimalistic shallow diffing algo here
-        child = child || new Component.HtmlElement(data.tagName, this, data.props);
-        childContent = child._compileContent(data.children);
-        // ... instead of emptying and rerendering
-        // we should iterate through children and remove or replace only those
-        // which are not marked for preservation
-        // don't want to do general diffing, as IMO this is more complicated
-        // as we need. The main concern for us is loosing selections or maybe
-        // flickering.
-        child.$el.empty();
-        child._renderContent(childContent);
-      } else if (data.type === 'class') {
-        // ... contrarily, we just use child.setProps in case
-        // of reusing a component declared via class
-        if (child) {
-          child.setProps(data.props);
-        } else {
-          child = new data.ComponentClass(this, data.props);
-          childContent = child._compileContent(child.render());
-          child._renderContent(childContent);
-        }
-      } else if (data.type === 'component') {
-        // ... and similar when reusing an instance based component
-        if (child) {
-          child.setProps(data.props);
-        } else {
-          child = data.component;
-          if (child.parent !== this) {
-            throw new Error('Child has not been created for this component.');
-          }
-          // render the component if it has never been rendered before
-          if (child._virgin) {
-            childContent = child._compileContent(child.render());
-            child._renderContent(childContent);
-          }
-        }
-      }
-      if (child) {
-        content.push(child);
-      }
-    }, this);
+    }
     return content;
+  }
+
+  this._renderContent = function(newContent) {
+    // to allow more freedom in Component.render()
+    // we do some normalization here
+    newContent = _prepareContent(newContent);
+
+    var el = this.$el[0];
+    var isMounted = _isInDocument(el);
+    var oldContent = this.content;
+    var oldComps = _indexByKey(oldContent, "old");
+    var newComps = _indexByKey(newContent);
+
+    var pos = 0;
+    var oldPos = 0;
+    var newPos = 0;
+
+    var children = {};
+    var refs = {};
+
+    function _removeOldData(key) {
+      // remove the data so we do not process it again
+      delete oldComps[key];
+      for (var i = oldPos+1; i < oldContent.length; i++) {
+        if (oldContent[i].component.keyId === key) {
+          oldContent.splice(i, 1);
+          break;
+        }
+      }
+    }
+
+    function _replace(oldComp, newComp) {
+      oldComp.triggerWillUnmount();
+      oldComp.$el.replaceWith(newComp.$el[0]);
+    }
+
+    function _update(comp, data) {
+      if (comp instanceof Component.HtmlElement) {
+        comp.setPropsAndChildren(data.props, data.children);
+      } else {
+        comp.setProps(data);
+      }
+    }
+
+    function _registerComponent(comp) {
+      children[comp.__id__] = comp;
+      if (comp.keyId) {
+        refs[comp.keyId] = comp;
+      } else if (comp.refId) {
+        refs[comp.refId] = comp;
+      }
+    }
+
+    // step through old and new content data (~virtual DOM)
+    // and apply changes to the component element
+    while(oldPos < oldContent.length || newPos < newContent.length) {
+      var node = el.childNodes[pos];
+      var _old = oldContent[oldPos];
+      var _new = newContent[newPos];
+      var comp = null;
+
+      // append remaining new components if there is no old one left
+      if (!_old) {
+        for (var i = newPos; i < newContent.length; i++) {
+          comp = this._compileComponent(newContent[i]);
+          if (isMounted) comp.triggerDidMount();
+          this.$el.append(comp.$el);
+          _registerComponent(comp);
+        }
+        break;
+      }
+      // unmount remaining old components if there is no old one left
+      if (!_new) {
+        for (var j = 0; j < oldContent.length; j++) {
+          oldContent[j].component.unmount();
+        }
+        break;
+      }
+
+      // otherwise do a differential update
+      if (node !== _old.component.$el[0]) {
+        throw new Error('Assertion failed: DOM structure is not as expected.');
+      }
+
+      // Note: if the key property is set the component is treated preservatively
+      var newKey = _new.props.key;
+      var oldKey = _old.component.keyId;
+      if (oldKey && newKey) {
+        // the component is in the right place already
+        if (oldKey === newKey) {
+          comp = _old.component;
+          _update(comp, _new);
+          pos++; oldPos++; newPos++;
+        }
+        // a new component has been inserted
+        else if (!oldComps[newKey] && newComps[oldKey]) {
+          comp = this._compileComponent(_new);
+          comp.$el.insertBefore(node);
+          if (isMounted) comp.triggerDidMount();
+          pos++; newPos++;
+        }
+        // old component has been replaced
+        else if (!oldComps[newKey] && !newComps[oldKey]) {
+          comp = this._compileComponent(_new);
+          _replace(_old.component, comp);
+          if (isMounted) comp.triggerDidMount();
+          newPos++; oldPos++;
+        }
+        // component has been moved to a different position
+        else if (oldComps[newKey]) {
+          comp = oldComps[newKey].component;
+          _update(comp, _new);
+          // if the old component is coming up components have been swapped
+          if (newComps[oldKey]) {
+            comp.$el.insertBefore(node);
+          }
+          // otherwise we can replace the old one
+          else {
+            _replace(_old.component, comp);
+            oldPos++;
+          }
+          pos++; newPos++;
+          // remove the data so we do not process it again
+          _removeOldData(newKey);
+        }
+        else {
+          throw new Error('Assertion failed: should not reach this statement.');
+        }
+      } else if (newKey) {
+        if (oldComps[newKey]) {
+          _old.component.unmount();
+          oldPos++;
+          // continueing as we did not insert a component
+          continue;
+        }
+        else {
+          comp = this._compileComponent(_new);
+          _replace(_old.component, comp);
+          if (isMounted) comp.triggerDidMount();
+          pos++; oldPos++; newPos++;
+        }
+      } else if (oldKey) {
+        comp = this._compileComponent(_new);
+        if (newComps[oldKey]) {
+          comp.$el.insertBefore(node);
+        } else {
+          _replace(_old.component, comp);
+          oldPos++;
+        }
+        if (isMounted) comp.triggerDidMount();
+        pos++; newPos++;
+      } else {
+        // do not replace text components if they are equal
+        if (_new.type === "text" && _old.type === "text" && _new.text === _old.text) {
+          // skip
+          pos++; oldPos++; newPos++;
+          continue;
+        }
+        comp = this._compileComponent(_new);
+        _replace(_old.component, comp);
+        if (isMounted) comp.triggerDidMount();
+        pos++; oldPos++; newPos++;
+      }
+
+      _registerComponent(comp);
+    }
+
+    this.children = children;
+    this.refs = refs;
+    this.content = newContent;
   };
 
-  this._renderContent = function(content) {
-    var $el = this.$el;
-    this.children = [];
-    this._childrenById = {};
-    this.refs = {};
-    _.each(content, function(child) {
-      if (_.isString(child)) {
-        $el.append(child);
-      } else {
-        this.children.push(child);
-        this._childrenById[child.__id__] = child;
-        if (child.refId) {
-          this.refs[child.refId] = child;
-        }
-        if (child.keyId) {
-          this._childrenById[child.keyId] = child;
-        }
-        $el.append(child.$el);
-      }
-    }, this);
-    delete this._virgin;
+  this._compileComponent = function(data) {
+    var component;
+    switch(data.type) {
+      case 'text':
+        component = new Component.Text(this, data.text);
+        break;
+      case 'element':
+        component = new Component.HtmlElement(this, data.tagName, data.props);
+        component._renderContent(data.children);
+        break;
+      case 'component':
+        component = new data.ComponentClass(this, data.props);
+        component._renderContent(component.render());
+        break;
+      default:
+        throw new Error('Illegal state.');
+    }
+    data.component = component;
+    return component;
   };
 
   this._collectContext = function() {
@@ -341,10 +479,17 @@ Component.Prototype = function ComponentPrototype() {
     return _.extend.apply(null, args);
   };
 
+
+  var _builtinProps = ['key', 'ref'];
+
   this._setProps = function(props) {
-    this.props = props || {};
+    props = props || {};
+    _.each(_builtinProps, function(key) {
+      delete props[key];
+    });
     // freezing properties to 'enforce' immutability
     Object.freeze(props);
+    this.props = props;
   };
 
   this._setState = function(state) {
@@ -362,11 +507,44 @@ Component.Root = function(props) {
 };
 OO.inherit(Component.Root, Component);
 
-Component.HtmlElement = function(tagName, parent, props) {
+Component.HtmlElement = function(parent, tagName, props) {
   this.tagName = tagName;
   Component.call(this, parent, props);
 };
+Component.HtmlElement.Prototype = function() {
+  this.setPropsAndChildren = function(props, children) {
+    if (children) {
+      this._setProps(props);
+      this._renderContent(children);
+    } else {
+      this.setProps(props);
+    }
+  };
+  this.setProps = function(newProps) {
+    // we do not need to rerender as new props can only be attributes
+    this.removeAttributes();
+    this._setProps(newProps);
+    this.setAttributes();
+  };
+};
 OO.inherit(Component.HtmlElement, Component);
+
+Component.Text = function(parent, text) {
+  Component.call(this, parent, {text: text});
+};
+Component.Text.Prototype = function() {
+  this.createElement = function() {
+    var el = document.createTextNode(this.props.text);
+    return $(el);
+  };
+  this.setProps = function(props) {
+    this._setProps(props);
+    var $oldEl = this.$el;
+    this.$el = this.createElement();
+    $oldEl.replaceWith(this.$el[0]);
+  };
+};
+OO.inherit(Component.Text, Component);
 
 Component.$$ = function() {
   var content = null;
@@ -383,28 +561,14 @@ Component.$$ = function() {
     };
   } else if (_.isFunction(arguments[0]) && arguments[0].prototype instanceof Component) {
     content = {
-      type: 'class',
-      ComponentClass: arguments[0],
-    };
-  } else if (arguments[0] instanceof Component) {
-    content = {
       type: 'component',
-      component: arguments[0],
+      ComponentClass: arguments[0],
     };
   } else {
     throw new Error('Illegal usage of Component.$$.');
   }
-  content.props = props;
-  content.children = children;
-  // propagate preservativeness
-  if (!content.isPreservative) {
-    for (var i = 0; i < children.length; i++) {
-      if (children[i].isPreservative) {
-        content.isPreservative = true;
-        break;
-      }
-    }
-  }
+  content.props = props || {};
+  content.children = children || [];
   return content;
 };
 
